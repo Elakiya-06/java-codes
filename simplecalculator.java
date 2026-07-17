5e8226a9aa0c32cb6e489e5b33fe621b408b83e2
Elakiya-06 <elakiyak03@gmail.com>
2026-07-17 14:49:35 +0530

    Create simplecalculator.java

---
+++ simplecalculator.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class Main {
+    public static void main(String[] args) {
+       Scanner sc=new Scanner(System.in);
+       System.out.print("Enter first Number:");
+       int a=sc.nextInt();
+       System.out.println("Enter second Number:");
+       int b=sc.nextInt();
+       System.out.println("Addition="+(a+b));
+       System.out.println("Subraction="+(a-b));
+       System.out.println("Multiplication="+(a+b));
+       System.out.println("Division="+(a/b));
+    }
+}
+    
+  
